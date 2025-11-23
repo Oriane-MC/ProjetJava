91682fd666a0088004e70fa0f370d010cd24cf9b
Oriane-MC <mainguychancogneoriane@gmail.com>
2025-11-23 16:27:47 +0100

    modif sur les programmes en rapport avec le calcul des points

--- src/Projet/Partie.java
+++ src/Projet/Partie.java
@@ -4,15 +4,16 @@ import java.util.*;
 
 public class Partie {
 	
-	//private ArrayList<Joueur> listJoueur;
+	private ArrayList<Joueur> listJoueur;
 	private LinkedList<Carte> listCarte;
-	//private List[Score] listScore;
 	private String etatPartie;
 	private int variantes;
+	private Trophee Trophees;
+	private Map<Joueur, Integer> Score = new HashMap();
 	
 	
 	public Partie() {
-		//this.listJoueur = new ArrayList();
+		this.listJoueur = new ArrayList();
 		this.listCarte = new LinkedList();
 		this.etatPartie = "en cours";
 		
@@ -31,20 +32,53 @@ public class Partie {
 		// peut etre on peut 'fussionner' la méthode mélanger et répartie car appelé que au début de partie
 		Carte c1 = listCarte.poll();
 		Carte c2 = listCarte.poll();
-		new Trophee(c1,c2);
+		this.Trophees = new Trophee(c1,c2);
 		new PaquetCarte(listCarte);
 	}
 	
-	//public void ajouterJoueur(Joueur j) {
-	//	this.listJoueur.add(j);
-	//}
+	public void ajouterJoueur(Joueur j) {
+		this.listJoueur.add(j);
+	}
+	
+	public void calculerScore() {
+
+		for (Carte t : this.Trophees.getListTrophee() ) {
+			
+			if (t.getCondition() == "Lowest") {
+				
+			}
+			else if (t.getCondition() == "Highest") {
+				
+			}
+			else if (t.getCondition() == "Majority") {
+				
+			}
+			else if (t.getCondition() == "Joker") {
+				
+			}
+			else if (t.getCondition() == "BestJest"){    // dans le cas ou on a besoin de calculer les points avant de donner les trophees 
+				// calculer les scores de chaque joueur et les enregistrer 
+				//les comparer 
+				// attribuer la carte au meilleur joueur
+				// recalculer le score de celui qui a changé de Jest 
+				// return pour ne pas recalculer 
+			}
+			else {
+				// calculer les scores de chaque joueur et les enregistrer en excluant celui qui a le joker 
+				// les comparer 
+				// attribuer la carte au meilleur joueur
+				// recalculer le score de celui qui a changé de Jest 
+				// return pour ne pas recalculer 
+			}
+			// calculer les scores pour chaque joueur
+		}
+	}
 	
 	
 	
 	public String toString() {
-		return "Partie [etatPartie= " + etatPartie + " ; Joueur = ; Paquet Carte" + listCarte +  
+		return "Partie [etatPartie= " + etatPartie + " ; Joueur = " + listJoueur + " ; Paquet Carte" + listCarte +  
 				"; variantes=" + variantes + "]";
-		//" + listJoueur + " 
 	}
 
 
--- src/Projet/Visitor.java
+++ src/Projet/Visitor.java
@@ -1,6 +1,7 @@
 package Projet;
 
-public class Visitor {
+public interface  Visitor {
 	// patron visitor : il va venir visiter chaque jouer et compter les point pour chacun des joueurs 
 
+	public void visiter(Joueur j);
 }
