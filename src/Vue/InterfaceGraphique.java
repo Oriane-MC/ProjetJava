b1e6ad451a671d009a006cd9df8005c4c6a7b3ff
Oriane-MC <mainguychancogneoriane@gmail.com>
2025-12-18 14:58:36 +0100

    ajout relatif au patron MVC

---
+++ src/Vue/InterfaceGraphique.java
@@ -0,0 +1,43 @@
+package Vue;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class InterfaceGraphique {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceGraphique window = new InterfaceGraphique();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InterfaceGraphique() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
